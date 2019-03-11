dd6cade22fd86d186e3acb889599bcaa4aec221a
eneax <19818937+eneax@users.noreply.github.com>
2019-03-11 22:11:32 +0100

    AddEntry - render

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 import { View } from "react-native"
 import { getMetricMetaInfo } from "../utils/helpers"
+import Slider from './Slider'
+import Steppers from './Steppers'
+
 
 export default class AddEntry extends Component {
   state = {
@@ -42,9 +45,33 @@ export default class AddEntry extends Component {
   }
 
   render() {
+    const metaInfo = getMetricMetaInfo()
+
     return (
       <View>
-        {getMetricMetaInfo("bike").getIcon()}
+        {Object.keys(metaInfo).map((key) => {
+          const { getIcon, type, ...rest } = metaInfo[key]
+          const value = this.state[key]
+
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider' 
+                ? <Slider 
+                    value={value} 
+                    onChange={(value) => this.slide(key, value)} 
+                    {...rest} 
+                  /> 
+                : <Steppers 
+                    value={value} 
+                    onIncrement={() => this.increment(key)} 
+                    onDecrement={() => this.decrement(key)} 
+                    {...rest} 
+                  />
+              }
+            </View>
+          )
+        })}
       </View>
     )
   }
