15c8647d00eeefd87c160ba96a000e2ee7e45653
eneax <19818937+eneax@users.noreply.github.com>
2019-03-14 22:45:46 +0100

    AddEntry - UdaciSlider

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import { View, TouchableOpacity, Text } from 'react-native'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
-import Slider from './Slider'
+import MySlider from './MySlider'
 import Steppers from './Steppers'
 import DateHeader from './DateHeader'
 
@@ -89,7 +89,7 @@ export default class AddEntry extends Component {
             <View key={key}>
               {getIcon()}
               {type === 'slider' 
-                ? <Slider 
+                ? <MySlider 
                     value={value} 
                     onChange={(value) => this.slide(key, value)} 
                     {...rest} 
