043ac5b16abbbf55f8e55ede126efd81a3c823d9
eneax <19818937+eneax@users.noreply.github.com>
2019-03-27 22:19:53 +0100

    History

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,12 @@
 import { AsyncStorage } from 'react-native'
-import { CALENDAR_STORAGE_KEY } from './_calendar'
+import { CALENDAR_STORAGE_KEY, formatCalendarResults } from './_calendar'
+
+
+export function fetchCalendarResults () {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+    .then(formatCalendarResults)
+}
+
 
 // 1. Submit entry for the day
 // 2. Add it to the 'DB'
