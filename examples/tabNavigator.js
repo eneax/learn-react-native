7052d32488dad26cef31d54bf8d1723f10f497bf
eneax <19818937+eneax@users.noreply.github.com>
2019-03-30 09:48:37 +0100

    Add TabNavigator example

---
+++ examples/tabNavigator.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation'
+
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+function Dashboard() {
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard</Text>
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarIcon: () => <FontAwesome name='home' size={30} color='black' />
+    }
+  },
+  Dashboard: {
+    screen: Dashboard,
+    navigationOptions: {
+      tabBarIcon: () => <FontAwesome name='dashboard' size={30} color='black' />
+    }
+  },
+})
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Tabs />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
