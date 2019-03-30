28b3eee45bdfdbe149c3004588958b1be0cdf370
eneax <19818937+eneax@users.noreply.github.com>
2019-03-30 10:04:48 +0100

    Add StatusBar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -8,6 +8,16 @@ import History from './components/History'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Constants } from 'expo'
+
+
+function MyStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 
 const RouteConfigs = {
@@ -60,7 +70,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <View style={{height: 20}} />
+          <MyStatusBar backgroundColor={purple} barStyle='light-content' />
           <TabsContainer />
         </View>
       </Provider>
