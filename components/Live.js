600b5ef3d0dc7dd224f5248028d7e58dc2812e6b
eneax <19818937+eneax@users.noreply.github.com>
2019-04-04 21:02:19 +0200

    Granted Geolocation

--- components/Live.js
+++ components/Live.js
@@ -7,7 +7,7 @@ import { purple, white } from '../utils/colors'
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'denied', // permissions granted by the user
+    status: 'granted', // permissions granted by the user
     direction: '',
   }
 
@@ -51,12 +51,34 @@ export default class Live extends Component {
 
     // granted permission
     return (
-      <View>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+      <View style={styles.container}>
+        <View style={styles.directionContainer}>
+        <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>
+            North
+          </Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, {color: white}]}>
+              Altitude
+            </Text>
+            <Text style={[styles.subHeader, {color: white}]}>
+              {200} feet
+            </Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, {color: white}]}>
+              Speed
+            </Text>
+            <Text style={[styles.subHeader, {color: white}]}>
+              {300} MPH
+            </Text>
+          </View>
+        </View>
       </View>
     )
-  }
+  }    
 }
 
 const styles = StyleSheet.create({
@@ -78,8 +100,41 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     margin: 20,
   },
-  buttonText :{
+  buttonText: {
     color: white,
     fontSize: 20,
-  }
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 35,
+    textAlign: 'center',
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: 'center',
+  },
+  metricContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: purple,
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 5,
+  },
 }) 
